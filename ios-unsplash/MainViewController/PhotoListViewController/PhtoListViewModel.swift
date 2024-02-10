61778963d423ff93862e8abcfeb511f0797cfa85
hemg2 <hemg2@naver.com>
2024-02-10 12:47:43 +0900

    feat: page 요청 추가

--- ios-unsplash/MainViewController/PhotoListViewController/PhtoListViewModel.swift
+++ ios-unsplash/MainViewController/PhotoListViewController/PhtoListViewModel.swift
@@ -12,26 +12,53 @@ final class PhotoListViewModel {
     private let repository: UnsplashRepository
     @Published var photos: [Photo] = []
     var cancellables: Set<AnyCancellable> = []
-    
-    var onPhotosUpdate: (() -> Void)?
+    var pageNumber: Int = 0
+    var isLoading: Bool = false
+    var isLastPage: Bool = false
     var onError: ((Error) -> Void)?
     
     init(repository: UnsplashRepository) {
         self.repository = repository
     }
     
-    func loadPhotos() {
-        repository.fetchPhotos()
-            .sink { completion in
+    func loadPhotos(isRefresh: Bool = false) {
+        guard !isLoading else { return }
+        
+        if isRefresh {
+            pageNumber = 1
+        } else {
+            pageNumber += 1
+        }
+        
+        isLoading = true
+        
+        repository.fetchPhotos(page: pageNumber)
+            .receive(on: DispatchQueue.main)
+            .sink(receiveCompletion: { [weak self] completion in
+                guard let self = self else { return }
+                self.isLoading = false
+                
                 switch completion {
-                case.failure(let error):
+                case .failure(let error):
                     self.onError?(error)
                 case .finished:
                     break
                 }
-            } receiveValue: { photos in
-                self.photos = photos
-            }
-            .store(in: &cancellables)
+            }, receiveValue: { [weak self] newPhotos in
+                guard let self = self else { return }
+                
+                if isRefresh {
+                    self.photos = newPhotos
+                } else {
+                    self.photos.append(contentsOf: newPhotos)
+                }
+                
+                if newPhotos.isEmpty {
+                    self.isLastPage = true
+                } else {
+                    self.pageNumber += 1
+                }
+            })
+            .store(in: &self.cancellables)
     }
 }
